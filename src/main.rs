76a64904173a1f75b3430cac54ef850dc7dc8319
jbloom-md <joseph@massdynamics.com>
2022-08-23 10:35:44 +1000

    use clap to parse cli args

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
+use clap::Parser;
+
+#[derive(Parser)]
 struct Cli {
+    // The pattern to look for
     pattern: String,
+    // The path to the file to read
+    #[clap(parse(from_os_str))]
     path: std::path::PathBuf,
 }
 
 
 fn main() {
-    let pattern = std::env::args().nth(1).expect("no pattern given");
-    let path  = std::env::args().nth(2).expect("no path given");
-    let args = Cli {
-            pattern: pattern,
-            path: std::path::PathBuf::from(path),
-        };
+    let args = Cli::parse();
+    
     println!("Hello, world!");
     println!("Searching for {}", args.pattern);
     println!("In file {}", args.path.display());
