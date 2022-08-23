5fd9de739557473f71d10c3f8abd2e551be236ec
jbloom-md <joseph@massdynamics.com>
2022-08-23 10:46:01 +1000

    print lines which contain the pattern

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,13 @@ struct Cli {
 
 fn main() {
     let args = Cli::parse();
-    
-    println!("Hello, world!");
-    println!("Searching for {}", args.pattern);
-    println!("In file {}", args.path.display());
+
+    let content = std::fs::read_to_string(&args.path)
+        .expect("could not read file");
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
 }
