941ba44a1c84363ff1bb4a5febfc16e4eb3945d9
jbloom-md <joseph@massdynamics.com>
2022-08-23 11:18:52 +1000

    we've now added error handling to find matches and a test for it

--- src/main.rs
+++ src/main.rs
@@ -14,10 +14,13 @@ struct Cli {
 }
 
 
-fn find_matches(content: &str, pattern: &str){
+fn find_matches(content: &str, pattern: &str,  mut writer: impl std::io::Write){
     for line in content.lines() {
         if line.contains(pattern) {
-            println!("{}", line);
+            match writeln!(writer, "{}", line) {
+                Ok(_) => (),
+                Err(e) => error!("Error writing to stdout: {}", e),
+            }
         }
     }
 }
@@ -31,8 +34,15 @@ fn main() -> Result<()> {
     let content = std::fs::read_to_string(&args.path)
         .with_context(|| format!("could not read file {:?}", &args.path))?;
 
-    find_matches(&content, &args.pattern);
+    find_matches(&content, &args.pattern, &mut std::io::stdout());
 
     info!("Shutting down");
     Ok(())
+}
+
+#[test]
+fn find_a_match() {
+    let mut result = Vec::new();
+    find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result);
+    assert_eq!(result, b"lorem ipsum\n");
 }
