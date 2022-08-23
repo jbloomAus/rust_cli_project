03347976a95e4f489e245bea0332f7c06eaabcdd
jbloom-md <joseph@massdynamics.com>
2022-08-23 10:57:45 +1000

    add nicer error reporting with anyhow

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ cargo run test test
 
 ## Learnings
 - VSCode started showing inferred types called "inlay hints". I updated my vscode to toggle these off when I press `ctrl+ alt` as they seem useful but possibly annoying if I can't toggle them. I'm curious whether I can auto-add them to code and if other crustaceans don't like/want that then why not? 
+- Anyhow library can be used to create a chain of errors  via the "context" trait. I like the sound of this because it sounds similar to the trace in python which I love. 
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use anyhow::{Context, Result};
 
 #[derive(Parser)]
 struct Cli {
@@ -10,15 +11,16 @@ struct Cli {
 }
 
 
-fn main() {
+fn main() -> Result<()> {
     let args = Cli::parse();
 
     let content = std::fs::read_to_string(&args.path)
-        .expect("could not read file");
+        .with_context(|| format!("could not read file {:?}", &args.path))?;
 
     for line in content.lines() {
         if line.contains(&args.pattern) {
             println!("{}", line);
         }
     }
+    Ok(())
 }
